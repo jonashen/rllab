9a6e0b600a32b0deafefa21829440c10a64307a2
Jonathon Shen <shenjona@usc.edu>
2018-04-26 13:05:52 -0700

    Fixed CPU bug

--- rllab/envs/mujoco/mujoco_env.py
+++ rllab/envs/mujoco/mujoco_env.py
@@ -74,18 +74,19 @@ class MujocoEnv(Env):
         self.qvel_dim = self.init_qvel.size
         self.ctrl_dim = self.init_ctrl.size
         self.action_noise = action_noise
-        if "frame_skip" in self.model.name_numericadr:
-            frame_skip_id = self.model.name_numericadr.index("frame_skip")
-            addr = self.sim.numeric_adr.flat[frame_skip_id]
-            self.frame_skip = int(self.sim.numeric_data.flat[addr])
-        else:
-            self.frame_skip = 1
-        if "init_qpos" in self.model.name_numericadr:
-            init_qpos_id = self.model.name_numericadr.index("init_qpos")
-            addr = self.sim.numeric_adr.flat[init_qpos_id]
-            size = self.sim.numeric_size.flat[init_qpos_id]
-            init_qpos = self.sim.numeric_data.flat[addr:addr + size]
-            self.init_qpos = init_qpos
+        self.frame_skip = 1
+        # if "frame_skip" in self.data.numeric_names:
+        #     frame_skip_id = self.data.numeric_names.index("frame_skip")
+        #     addr = self.sim.numeric_adr.flat[frame_skip_id]
+        #     self.frame_skip = int(self.sim.numeric_data.flat[addr])
+        # else:
+        #     self.frame_skip = 1
+        # if "init_qpos" in self.data.numeric_names:
+        #     init_qpos_id = self.data.numeric_names.index("init_qpos")
+        #     addr = self.sim.numeric_adr.flat[init_qpos_id]
+        #     size = self.sim.numeric_size.flat[init_qpos_id]
+        #     init_qpos = self.sim.numeric_data.flat[addr:addr + size]
+        #     self.init_qpos = init_qpos
         self.dcom = None
         self.current_com = None
         self.reset()
@@ -220,7 +221,7 @@ class MujocoEnv(Env):
 
     def stop_viewer(self):
         if self.viewer:
-            self.viewer
+            self.viewer.finish()
 
     def release(self):
         # temporarily alleviate the issue (but still some leak)
